e972d72329d5fa369d1503a51a24ad1fd22720d6
Mateus Vasconcelos <161360131+Mateus0Vasconcelos@users.noreply.github.com>
2025-10-14 20:54:45 -0300

    Update toggle.js

--- Prototipos-de-tela/CSU 1/toggle.js
+++ Prototipos-de-tela/CSU 1/toggle.js
@@ -8,15 +8,16 @@ toggleLinkEl.addEventListener("click", () => {
         // troca para login
         loginEl.classList.remove("campo-inativo");
         cadastroEl.classList.add("campo-inativo");
-        textoLinkEl.innerHTML = "Ainda não é cliente?";
+        textoLinkEl.innerHTML = "Ainda não é cliente? ";
         toggleLinkEl.innerHTML = "Cadastre-se";
         textoLinkEl.appendChild(toggleLinkEl);
     } else {
         // troca para cadastro
         loginEl.classList.add("campo-inativo");
         cadastroEl.classList.remove("campo-inativo");
-        textoLinkEl.innerHTML = "Já é cliente?";
+        textoLinkEl.innerHTML = "Já é cliente? ";
         toggleLinkEl.innerHTML = "Entre";
         textoLinkEl.appendChild(toggleLinkEl);
     }
+
 });
